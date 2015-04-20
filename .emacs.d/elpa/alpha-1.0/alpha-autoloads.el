0b887c92925a860338088dcdd6c62c915e5c714d
Kamran <team.radiant@gmail.com>
2015-04-20 08:32:55 +0530

    Initial commit

---
+++ .emacs.d/elpa/alpha-1.0/alpha-autoloads.el
@@ -0,0 +1,18 @@
+;;; alpha-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("alpha-pkg.el" "alpha.el") (21810 16081
+;;;;;;  885165 464000))
+
+;;;***
+
+(provide 'alpha-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; alpha-autoloads.el ends here
---
+++ .emacs.d/elpa/color-theme-cobalt-0.0.2/color-theme-cobalt-autoloads.el
@@ -0,0 +1,29 @@
+;;; color-theme-cobalt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (color-theme-cobalt) "color-theme-cobalt" "color-theme-cobalt.el"
+;;;;;;  (21810 14460 795855 580000))
+;;; Generated autoloads from color-theme-cobalt.el
+
+(autoload 'color-theme-cobalt "color-theme-cobalt" "\
+Color theme by Nick Ewing, based off the TextMate Cobalt theme, created 2010-04-16
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("color-theme-cobalt-pkg.el") (21810 14460
+;;;;;;  889401 597000))
+
+;;;***
+
+(provide 'color-theme-cobalt-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; color-theme-cobalt-autoloads.el ends here
