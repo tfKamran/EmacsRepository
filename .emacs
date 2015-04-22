33ed62bde0f39f41e9c30e2e36da2c7d0ec9de3c
Kamran <team.radiant@gmail.com>
2015-04-22 09:46:56 +0530

    Install ac packages for html, meta-presenter, myterminal-controls

--- .emacs
+++ .emacs
@@ -24,6 +24,12 @@
 ;(package-install 'powerline)
 ;(package-install 'php+-mode)
 ;(package-install 'ac-php)
+;(package-install 'myterminal-controls)
+;(package-install 'ac-html)
+;(package-install 'ac-html-bootstrap)
+;(package-install 'ac-html-csswatcher)
+;(package-install 'web-mode)
+;(package-install 'meta-presenter)
 
 (load-theme 'wombat)
 (menu-bar-mode -1)
@@ -65,3 +71,17 @@
 (global-set-key (kbd "C-M-{") 'transparency-decrease)
 (global-set-key (kbd "C-}") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-{") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-M-'") 'myterminal-controls-open-controls)
+
+(myterminal-controls-set-controls-data 
+ (list '("1" "Toggle minimap" (lambda ()
+                                 (minimap-toggle)))
+       '("2" "Toggle tool-bar" (lambda ()
+                                 (cond (tool-bar-mode (tool-bar-mode -1))
+                                       (t (tool-bar-mode t)))))
+       '("3" "Toggle scroll-bar" (lambda ()
+                                   (cond (scroll-bar-mode (scroll-bar-mode -1))
+                                         (t (scroll-bar-mode t)))))
+       '("4" "Invert colors" (lambda ()
+                               (invert-face 'default)))))
+
---
+++ .emacs.d/elpa/ac-html-20150118.2116/ac-html-autoloads.el
@@ -0,0 +1,67 @@
+;;; ac-html-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ac-haml-enable) "ac-haml" "ac-haml.el" (21815
+;;;;;;  7952 480161 514000))
+;;; Generated autoloads from ac-haml.el
+
+(autoload 'ac-haml-enable "ac-haml" "\
+Add ac-haml sources into ac-sources and enable auto-comple-mode
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (ac-html-enable) "ac-html" "ac-html.el" (21815
+;;;;;;  7952 432161 501000))
+;;; Generated autoloads from ac-html.el
+
+(defvar ac-source-html-tag '((candidates . ac-source-html-tag-candidates) (prefix . "<\\(.*\\)") (symbol . "t")))
+
+(defvar ac-source-html-attribute '((candidates . ac-source-html-attribute-candidates) (prefix . "<\\w[^>]*[[:space:]]+\\(.*\\)") (symbol . "a")))
+
+(defvar ac-source-html-attribute-value '((candidates . ac-source-html-attribute-value-candidates) (prefix . ac-html-value-prefix) (document . ac-source-html-attribute-value-document) (symbol . "v")))
+
+(autoload 'ac-html-enable "ac-html" "\
+Add ac-html sources into ac-sources and enable auto-comple-mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (ac-jade-enable) "ac-jade" "ac-jade.el" (21815
+;;;;;;  7952 400161 493000))
+;;; Generated autoloads from ac-jade.el
+
+(autoload 'ac-jade-enable "ac-jade" "\
+Add ac-jade sources into ac-sources and enable auto-comple-mode
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (ac-slim-enable) "ac-slim" "ac-slim.el" (21815
+;;;;;;  7921 72133 364000))
+;;; Generated autoloads from ac-slim.el
+
+(autoload 'ac-slim-enable "ac-slim" "\
+Add ac-slim sources into ac-sources and enable auto-comple-mode
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-html-pkg.el") (21815 7952 524339 337000))
+
+;;;***
+
+(provide 'ac-html-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ac-html-autoloads.el ends here
